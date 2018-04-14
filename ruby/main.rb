5b77497e912744b99c1e4dc30f60ca3ea45fb5c6
Apostolos Sotiropoulos <apostolis@theartwishlist.com>
2018-04-14 20:52:04 +0300

    scope n inheritance for ruby

--- ruby/main.rb
+++ ruby/main.rb
@@ -9,15 +9,72 @@
 def change_string(str)
   # the value of str is a reference to a mutable (not frozen) string object
   # << is altering this object
-  str << "by reference."
+  str << 'by reference.'
   # local var str will now contain a reference to another string object
-  str = "this will never be shown"
+  str = 'this will never be shown'
 end
 
-original_str = "-"
+original_str = '-'
 change_string(original_str)
-puts "Function Args: Pass by value or by reference?"
+puts 'Function Args: Pass by value or by reference?'
 puts original_str
 
-puts "-But we need to keep in mind that there are mutable and immutable objects." 
-puts "-42, 42.0 and true are immutable objects. You can check with 42.frozen?"
+puts '-But caution: there are mutable and immutable objects.'
+puts '-42, 42.0 and true are immutable objects. You can check with 42.frozen?'
+
+# Scope?
+# Resources:
+# https://www.sitepoint.com/understanding-scope-in-ruby/
+# https://jacopretorius.net/2012/01/block-variable-scope-in-ruby.html
+class Vehicle
+  attr_reader :name # defines a class method name for class var name
+  PURPOSE = 'transfer things and humans'.freeze # constant
+  @@type = 'Vehicle' # class variable
+
+  # class method
+  def self.type
+    @@type
+  end
+
+  def initialize(name)
+    # instance variable : every instance can have a different value for it
+    @name = name
+  end
+
+  # instance method
+  def start
+    puts "#{@@type} named #{@name} just started trying to #{PURPOSE}."
+  end
+end
+
+some_vehicle = Vehicle.new 'fiab'
+puts 'Scope?'
+puts "-Class variable (i.e @@type = #{Vehicle.type}): Available from the\
+ class definition and any sub-classes. Not available from anywhere outside."
+puts "-Instance variable (i.e @name = #{some_vehicle.name}): Available\
+ only within a specific object, across all methods in a class instance.\
+ Not available directly from class definitions."
+puts '-Global variable ($a_variable): Available everywhere within the script.'
+puts '-Local variable (a_variable): It depends on the scope.'
+puts '-- local variables created in the blocks are not available outside them'
+puts '-- local variables created outside the block are available within them'
+
+# Inheritance?
+# Resources:
+# https://learnrubythehardway.org/book/ex44.html
+class Car < Vehicle
+  def initialize(name, doors = 2)
+    super name
+    @doors = doors
+  end
+
+  def two_doored?
+    @doors == 2
+  end
+end
+
+some_car = Car.new 'aubi', 4
+puts 'Inheritance?'
+puts '-easy. For instance lets try starting my car: '
+some_car.start
+puts '-and btw this is a 2 doored car' if some_car.two_doored? # won't show
